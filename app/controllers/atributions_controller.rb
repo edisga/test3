1bf88f6aa296bc219a9b41164e63327b73ec12a4
Jose Hernandez <pepeh93@hotmail.es>
2021-10-20 18:53:49 +0200

    created controllers

---
+++ app/controllers/atributions_controller.rb
@@ -0,0 +1,2 @@
+class AtributionsController < ApplicationController
+end
---
+++ test/controllers/atributions_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AtributionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
