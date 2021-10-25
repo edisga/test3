8292985ab8f29c0eda43287386b8f29fbb204345
Jose Hernandez <pepeh93@hotmail.es>
2021-10-26 01:34:26 +0200

    push to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,41 @@ require 'faker'
 
 puts "Cleaning all DB"
 Answer.destroy_all
-Atribution.destroy_all
 Course.destroy_all
 Lesson.destroy_all
 Modul.destroy_all
 Question.destroy_all
-Requirement.destroy_all
 Section.destroy_all
 Teacher.destroy_all
 User.destroy_all
 puts "DB cleanded!"
 
 puts "Creating new DB!"
+puts "Creating user..."
+
+user = User.new (
+  email: "joseluis@gmail.com",
+  password: "Unmomentojavi"
+)
+
+user.save!
+puts "#{user.email} created!"
+
+puts "Creating Teacher..."
+
+teacher = Teacher.new (
+  name: "Carolina",
+  last_name: "Lacasta",
+  description: "Soy una loba de mar. He navegado más millas que la embarcación Gusanito",
+  email: "carolina@gmail.com",
+  password: "Tratamebien"
+)
+
+teacher.save!
+puts "#{teacher.name} created"
+
+puts "Creating Courses"
+
+5.times do
+  
 
