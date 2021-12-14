857cc0857977686010b0cf08a3dd8c76fa74ff55
Jose Hernandez <pepeh93@hotmail.es>
2021-12-14 01:07:49 +0100

    added controllers

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,41 @@
 class LessonsController < ApplicationController
+  before_action :find_lesson, only: [:show, :edit, :update, :delete]
+  before_action :find_module, only: [:show, :edit, :update, :index, :new, :create]
+  def index
+    @lessons = @moduls.lessons
+  end
+  
+  def new
+  
+  end
+  
+  def create
+  
+  end
+  
+  def edit
+  
+  end
+  
+  def update
+  
+  end
+  
+  def show
+  
+  end
+  
+  def destroy
+  
+  end
+
+  private
+
+  def find_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def find_module
+    @modul = Modul.find(params[:modul_id])
+  end
 end
