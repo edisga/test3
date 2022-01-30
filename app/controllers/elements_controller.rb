06b99f39fc48b11648a5d5993073b1a4b44c7876
Jose Hernandez <pepeh93@hotmail.es>
2022-01-30 18:07:08 +0100

    Solved save content :

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -55,6 +55,6 @@ class ElementsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def element_params
-      params.require(:element).permit(:element_type, :section_id, :position)
+      params.require(:element).permit(:element_type, :content, :image)
     end
 end
